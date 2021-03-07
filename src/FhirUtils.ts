7ee575fd34644bbd78524d2b7b37fbd0c1da86c9
Fernando <fernandolatorre@conectate-soluciones.com>
2021-03-07 18:40:50 +0100

    added class Immunization

--- src/FhirUtils.ts
+++ src/FhirUtils.ts
@@ -4,17 +4,20 @@ import { Attachment } from "./fhirUtils/Attachment";
 import { Bundle } from "./fhirUtils/Bundle";
 import { BundleMessage } from "./fhirUtils/BundleMessage";
 import { CodeableConcept } from "./fhirUtils/CodeableConcept";
+import { Immunization } from "./fhirUtils/Immunization";
 
 export class FhirUtils {
     public attachment: Attachment
     public bundle: Bundle
     public bundleMessage: BundleMessage
     public codeableConcept: CodeableConcept
+    public immunization: Immunization
 
     constructor() {
         this.attachment = new Attachment()
         this.bundle = new Bundle()
         this.bundleMessage = new BundleMessage()
         this.codeableConcept = new CodeableConcept()
+        this.immunization = new Immunization()
     }
 }
