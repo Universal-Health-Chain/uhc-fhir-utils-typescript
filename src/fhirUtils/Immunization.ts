7ba83d7e582ffbc7a9a3a624170c8aaa0d9f27c0
Fernando <fernandolatorre@conectate-soluciones.com>
2021-03-07 18:25:17 +0100

    Immunization system

--- src/fhirUtils/Immunization.ts
+++ src/fhirUtils/Immunization.ts
@@ -1,14 +1,14 @@
 
 /* Copyright 2020-2021 FUNDACION UNID. Apache License 2.0 */
 
-export const systemVaccineATC = "http://www.whocc.no/atc"
-export const systemVaccineCPT = "http://www.ama-assn.org/go/cpt"
-export const systemVaccineCVX = "http://hl7.org/fhir/sid/cvx"
-export const systemVaccineSNOMED = "http://snomed.info/sct"    // http://hl7.org/fhir/uv/ips/ValueSet-vaccines-gps-uv-ips.html
-export const systemImmunizationFunding = "http://terminology.hl7.org/CodeSystem/immunization-funding-source"
-export const systemImmunizationEligibilty = "http://terminology.hl7.org/CodeSystem/immunization-program-eligibility"
-export const systemImmunizationOrigin = "http://terminology.hl7.org/CodeSystem/immunization-origin"
-export const systemNotAdministeredReason = "http://terminology.hl7.org/CodeSystem/v3-ActReason"
-export const systemAdministrationSite = "http://terminology.hl7.org/CodeSystem/v3-ActSite"
-export const systemRouteOfAdministration = "http://terminology.hl7.org/CodeSystem/v3-RouteOfAdministration"
-export const systemImmunizationSubpotent = "http://terminology.hl7.org/CodeSystem/immunization-subpotent-reason"
+export const systemVaccineATC               = () => "http://www.whocc.no/atc"
+export const systemVaccineCPT               = () => "http://www.ama-assn.org/go/cpt"
+export const systemVaccineCVX               = () => "http://hl7.org/fhir/sid/cvx"
+export const systemVaccineSNOMED            = () => "http://snomed.info/sct"    // http://hl7.org/fhir/uv/ips/ValueSet-vaccines-gps-uv-ips.html
+export const systemImmunizationFunding      = () => "http://terminology.hl7.org/CodeSystem/immunization-funding-source"
+export const systemImmunizationEligibilty   = () => "http://terminology.hl7.org/CodeSystem/immunization-program-eligibility"
+export const systemImmunizationOrigin       = () => "http://terminology.hl7.org/CodeSystem/immunization-origin"
+export const systemNotAdministeredReason    = () => "http://terminology.hl7.org/CodeSystem/v3-ActReason"
+export const systemAdministrationSite       = () => "http://terminology.hl7.org/CodeSystem/v3-ActSite"
+export const systemRouteOfAdministration    = () => "http://terminology.hl7.org/CodeSystem/v3-RouteOfAdministration"
+export const systemImmunizationSubpotent    = () => "http://terminology.hl7.org/CodeSystem/immunization-subpotent-reason"
