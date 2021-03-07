99f37c836110260b1b909e364d158b4eb3cf3d07
Fernando <fernandolatorre@conectate-soluciones.com>
2021-03-07 18:43:32 +0100

    added libs

--- lib/FhirUtils.d.ts
+++ lib/FhirUtils.d.ts
@@ -2,10 +2,12 @@ import { Attachment } from "./fhirUtils/Attachment";
 import { Bundle } from "./fhirUtils/Bundle";
 import { BundleMessage } from "./fhirUtils/BundleMessage";
 import { CodeableConcept } from "./fhirUtils/CodeableConcept";
+import { Immunization } from "./fhirUtils/Immunization";
 export declare class FhirUtils {
     attachment: Attachment;
     bundle: Bundle;
     bundleMessage: BundleMessage;
     codeableConcept: CodeableConcept;
+    immunization: Immunization;
     constructor();
 }
---
+++ lib/fhirUtils/Immunization.d.ts
@@ -0,0 +1,14 @@
+export declare class Immunization {
+    constructor();
+    systemVaccineATC: () => string;
+    systemVaccineCPT: () => string;
+    systemVaccineCVX: () => string;
+    systemVaccineSNOMED: () => string;
+    systemImmunizationFunding: () => string;
+    systemImmunizationEligibilty: () => string;
+    systemImmunizationOrigin: () => string;
+    systemNotAdministeredReason: () => string;
+    systemAdministrationSite: () => string;
+    systemRouteOfAdministration: () => string;
+    systemImmunizationSubpotent: () => string;
+}
