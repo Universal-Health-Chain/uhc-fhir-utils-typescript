2221fbd4daa664af64ddf015af859c790c038a71
dverma710 <dverma@connecthealth.info>
2023-08-02 14:05:47 -0400

    DiagnosticReportModel and DetectedIssueModel

---
+++ src/models/fhirResourceModels/DetectedIssueModel.ts
@@ -0,0 +1,71 @@
+import { URLSearchParams } from 'url';
+
+// DetectedIssue Parameters
+export enum DetectedIssueParameters {
+    Author = 'author',
+    Code = 'code',
+    Identified = 'identified',
+    Identifier = 'identifier',
+    Implicated = 'implicated',
+    Patient = 'patient',
+    Status = 'status',
+  }
+  
+  // DetectedIssue Resource Interface
+  export interface DetectedIssueResource {
+    [DetectedIssueParameters.Author]?: ReferenceSearchParameter;
+    [DetectedIssueParameters.Code]?: TokenSearchParameter;
+    [DetectedIssueParameters.Identified]?: DateSearchParameter;
+    [DetectedIssueParameters.Identifier]?: TokenSearchParameter;
+    [DetectedIssueParameters.Implicated]?: ReferenceSearchParameter;
+    [DetectedIssueParameters.Patient]?: ReferenceSearchParameter;
+    [DetectedIssueParameters.Status]?: TokenSearchParameter;
+  }
+  
+  // DetectedIssue Class
+  export class DetectedIssue {
+    resourceData: DetectedIssueResource;
+    fhirRelease: string;
+  
+    constructor(url: string, fhirRelease: string = 'r4') {
+      this.fhirRelease = fhirRelease;
+      this.resourceData = {};
+      this.fromUrlParams(url);
+    }
+  
+    fromUrlParams(url: string) {
+      const urlParams = new URLSearchParams(url);
+      for (const [key, value] of urlParams.entries()) {
+        if (key in DetectedIssueParameters) {
+          let parameter: SearchParameter;
+          switch (key) {
+            case DetectedIssueParameters.Author:
+            case DetectedIssueParameters.Code:
+            case DetectedIssueParameters.Implicated:
+            case DetectedIssueParameters.Patient:
+            case DetectedIssueParameters.Status:
+              parameter = parseTokenParameter(value);
+              break;
+            case DetectedIssueParameters.Identified:
+              parameter = parseDateParameter(value);
+              break;
+            case DetectedIssueParameters.Identifier:
+              parameter = parseReferenceParameter(value);
+              break;
+            default:
+              throw new Error(`Unsupported parameter: ${key}`);
+          }
+          parameter.name = key;
+          parameter.base = ['DetectedIssue'];
+          this.resourceData[key as keyof DetectedIssueResource] = parameter;
+        }
+      }
+    }
+  
+    updateResourceData(newData: DetectedIssueResource) {
+      this.resourceData = { ...this.resourceData, ...newData };
+    }
+  
+    // Method to convert the resourceData to FHIR resource goes here...
+  }
+  
---
+++ src/models/fhirResourceModels/DiagnosticReportModel.ts
@@ -0,0 +1,83 @@
+import { URLSearchParams } from 'url';
+
+// DiagnosticReport Parameters
+export enum DiagnosticReportParameters {
+    BasedOn = 'based-on',
+    Category = 'category',
+    Code = 'code',
+    Date = 'date',
+    Encounter = 'encounter',
+    Identifier = 'identifier',
+    ImagingStudy = 'imaging-study',
+    Patient = 'patient',
+    Performer = 'performer',
+    Result = 'result',
+    Status = 'status',
+  }
+  
+  // DiagnosticReport Resource Interface
+  export interface DiagnosticReportResource {
+    [DiagnosticReportParameters.BasedOn]?: ReferenceSearchParameter;
+    [DiagnosticReportParameters.Category]?: TokenSearchParameter;
+    [DiagnosticReportParameters.Code]?: TokenSearchParameter;
+    [DiagnosticReportParameters.Date]?: DateSearchParameter;
+    [DiagnosticReportParameters.Encounter]?: ReferenceSearchParameter;
+    [DiagnosticReportParameters.Identifier]?: TokenSearchParameter;
+    [DiagnosticReportParameters.ImagingStudy]?: ReferenceSearchParameter;
+    [DiagnosticReportParameters.Patient]?: ReferenceSearchParameter;
+    [DiagnosticReportParameters.Performer]?: ReferenceSearchParameter;
+    [DiagnosticReportParameters.Result]?: ReferenceSearchParameter;
+    [DiagnosticReportParameters.Status]?: TokenSearchParameter;
+  }
+  
+  // DiagnosticReport Class
+  export class DiagnosticReport {
+    resourceData: DiagnosticReportResource;
+    fhirRelease: string;
+  
+    constructor(url: string, fhirRelease: string = 'r4') {
+      this.fhirRelease = fhirRelease;
+      this.resourceData = {};
+      this.fromUrlParams(url);
+    }
+  
+    fromUrlParams(url: string) {
+      const urlParams = new URLSearchParams(url);
+      for (const [key, value] of urlParams.entries()) {
+        if (key in DiagnosticReportParameters) {
+          let parameter: SearchParameter;
+          switch (key) {
+            case DiagnosticReportParameters.BasedOn:
+            case DiagnosticReportParameters.Category:
+            case DiagnosticReportParameters.Code:
+            case DiagnosticReportParameters.Encounter:
+            case DiagnosticReportParameters.ImagingStudy:
+            case DiagnosticReportParameters.Patient:
+            case DiagnosticReportParameters.Performer:
+            case DiagnosticReportParameters.Result:
+            case DiagnosticReportParameters.Status:
+              parameter = parseTokenParameter(value);
+              break;
+            case DiagnosticReportParameters.Date:
+              parameter = parseDateParameter(value);
+              break;
+            case DiagnosticReportParameters.Identifier:
+              parameter = parseReferenceParameter(value);
+              break;
+            default:
+              throw new Error(`Unsupported parameter: ${key}`);
+          }
+          parameter.name = key;
+          parameter.base = ['DiagnosticReport'];
+          this.resourceData[key as keyof DiagnosticReportResource] = parameter;
+        }
+      }
+    }
+  
+    updateResourceData(newData: DiagnosticReportResource) {
+      this.resourceData = { ...this.resourceData, ...newData };
+    }
+  
+    // Method to convert the resourceData to FHIR resource goes here...
+  }
+  
